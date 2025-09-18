4a87718f102be23b1e1379c4b9292f381c4ce702
bfite <bfite@medanswering.com>
2025-09-18 11:30:12 -0400

    bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_17_201933) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_18_151931) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -24,5 +24,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_17_201933) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
